deec795cc61e859c5dbfcf67c983d671ddeca58a
galind <guillem.yt1@gmail.com>
2024-10-26 18:26:52 +0200

    implemented sold check

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from utils import setup_console_logger, format_watch_reference, read_csv, any_wo
 logger = setup_console_logger(logging.INFO)
 
 
-def search_in_ebay(watch_reference: str) -> Optional[list]:
+def search_in_ebay(watch_reference: str, check_sold: bool) -> Optional[list]:
     session = requests.Session()
     advanced_search_endpoint = f"https://www.ebay.es/sch/i.html?_nkw={watch_reference}&_in_kw=3&_sacat=281"
+    if check_sold:
+        advanced_search_endpoint += "&LH_Sold=1&LH_Complete=1"
     logger.debug(f"{watch_reference} | URL: {advanced_search_endpoint}")
 
     response = session.get(advanced_search_endpoint, allow_redirects=True)
@@ -52,16 +54,42 @@ def run() -> None:
     content = read_csv("watches.csv")
     for year, reference, name, quantity in content:
         formated_reference = format_watch_reference(reference)
-        results = search_in_ebay(formated_reference)
+        not_sold_results = search_in_ebay(formated_reference, check_sold=False)
+        sold_results = search_in_ebay(formated_reference, check_sold=True)
 
-        if not results:
+        if not not_sold_results and not sold_results:
             logger.error(f"{formated_reference} | Watch not found, skipping...")
             continue
 
-        price_average = round(sum(results) / len(results), 2)
-        amount_total += price_average * int(quantity)
+        if not_sold_results:
+            not_sold_price_average = round(sum(not_sold_results) / len(not_sold_results), 2)
+            logger.debug(f"{formated_reference} | Not sold average: {not_sold_price_average}")
+        else:
+            not_sold_price_average = None
+
+        if sold_results:
+            sold_price_average = round(sum(sold_results) / len(sold_results), 2)
+            logger.debug(f"{formated_reference} | Sold average: {sold_price_average}")
+        else:
+            sold_price_average = None
+
+        if not_sold_price_average and sold_price_average:
+            total_average = round((not_sold_price_average + sold_price_average) / 2, 2)
+        elif sold_price_average:
+            total_average = sold_price_average
+        elif not_sold_price_average:
+            total_average = not_sold_price_average
+        else:
+            continue
+        logger.debug(f"{formated_reference} | Total average: {total_average}")
+
+        amount_total += total_average * int(quantity)
         found_watches += 1
-        logger.info(f"{formated_reference} | {found_watches}/{len(content)} | Average: {price_average}")
+
+        qty_text = f"{quantity} units" if int(quantity) > 1 else f"{quantity} unit"
+        logger.info(
+            f"{formated_reference} | {found_watches}/{len(content)} | Watch price: {total_average} ({qty_text})"
+        )
 
     logger.info(
         f"Estimate total value of the collection ({found_watches}/{len(content)} watches found): "
